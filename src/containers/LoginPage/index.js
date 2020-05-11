f1ee6cd9a44b828aeb3538131713948043033740
Erika-Skarda <erikaskarda@yahoo.com.br>
2020-05-11 13:50:48 -0300

    Login e Sign Up sem formatação

---
+++ src/containers/LoginPage/index.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { routes } from "../Router";
+
+const FormLogin = [
+    {
+        name:"email",
+        type:"email",
+        placeholder:"email@email.com",
+        label:"E-mail*",
+        required:true,
+        title: "Campo obrigatório",
+        pattern:"[a-z0-9_.+-%]+@[a-z0-9.-]+\.[a-z]{3,}$" 
+    },
+    {
+        name:"password",
+        type:"password",
+        placeholder:"Mínimo 6 caracteres",
+        label:"Senha*",
+        title:"Digite no mínimo 6 caracteres",
+        required:true,
+        pattern:"[A-Za-z0-9]{6,10}",
+        
+    }
+]
+class LoginPage extends Component {
+    constructor(props) {
+    super(props)
+
+    this.state =  {
+        form: {
+            email: "",
+            password: ""
+        }
+    }}
+handleOnChangeForm = event => {
+    const { name, value} = event.target;
+    this.setState({ form: {...this.state.form,[name]:value}})
+}
+handleOnSubmit = event => {
+    event.preventDefault()
+
+}
+
+render () {
+ return (
+    <>
+    <form onSubmit={this.handleOnSubmit}>
+        {FormLogin.map(info => {
+            return (
+                <div key={info.name}>
+                <label htmlFor={info.name}>{info.name}</label>
+                    <input
+                        name={info.name}
+                        placeholder={info.placeholder}
+                        type={info.type}
+                        value={this.state.form[info.name] || ""}
+                        required={info.required}
+                        onChange={this.handleOnChangeForm}
+                        pattern={info.pattern}
+                        title={info.title}
+
+                    />
+
+                </div>)
+        })}
+        <button type="submit">Entrar</button>
+    </form>
+    </> 
+ )
+
+}
+
+}
+
+export default connect()(LoginPage)
--- src/containers/Router/index.js
+++ src/containers/Router/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { ConnectedRouter } from "connected-react-router";
 import { Switch, Route } from "react-router-dom";
+import LoginPage from "../LoginPage";
+import SignUpPage from "../SignUpPage"
 
-const routes = {};
+const routes = {
+  root:"/",
+  signup:"/signup"
+};
 
 function Router(props) {
   return (
     <ConnectedRouter history={props.history}>
       <Switch>
-        <Route path={routes.root} component={() => <div />} />
+        <Route path={routes.root} component={LoginPage} />
+        <Route path={routes.signup} component={SignUpPage} />
       </Switch>
     </ConnectedRouter>
   );
