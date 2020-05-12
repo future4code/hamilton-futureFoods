763e9312c040454a566749b2d27d8fae2777e3b1
Erika-Skarda <erikaskarda@yahoo.com.br>
2020-05-12 13:20:13 -0300

    My Adress

--- src/containers/Router/index.js
+++ src/containers/Router/index.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { ConnectedRouter } from "connected-react-router";
 import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 import LoginPage from "../LoginPage";
-import SignUpPage from "../SignUpPage"
+import SignUpPage from "../SignUpPage";
+import MyAdress from "../MyAdress"
 
 const routes = {
   root:"/",
-  signup:"/signup"
+  signup:"/signup",
+  myadress:"/myadress"
 };
 
 function Router(props) {
   return (
     <ConnectedRouter history={props.history}>
       <Switch>
-        <Route path={routes.root} component={LoginPage} />
-        <Route path={routes.signup} component={SignUpPage} />
+        <Route exact path={routes.root} component={LoginPage} />
+        <Route exact path={routes.signup} component={SignUpPage} />
+        <Route exact path={routes.myadress} component={MyAdress} />
       </Switch>
     </ConnectedRouter>
   );
 }
 
-export default Router;
+export default connect() (Router);
