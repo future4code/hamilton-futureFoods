8a9d69b765435536bb9006c93626d390d93ae54f
skempfer <shana.kempfer88@gmail.com>
2020-05-13 14:51:03 -0300

    actions e reducers do feed

---
+++ src/reducers/feed.js
@@ -0,0 +1,19 @@
+const initialState = {
+    allRestaurants: [],
+    selectedRestaurantId: "",
+    restaurantDetails: {},
+}
+
+const feed = (state = initialState, action) => {
+    console.log(action.payload)
+    switch(action.type) {
+        case 'SET_ALL_RESTAURANTS':
+            const restaurantList = action.payload.allRestaurants;
+            return {...state, allRestaurants: restaurantList};
+        case 'SET_RESTAURANTS_DETAILS':
+            const restaurantsDetails = action.Payload.restaurantsDetails;
+            return {...state, restaurantsDetails: restaurantsDetails};
+        default:
+            return: state;
+    }
+}
